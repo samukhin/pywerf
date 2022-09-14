575ab5d9c31fcdae719e428a11e519165bd57d6f
samukhin <47834890+samukhin@users.noreply.github.com>
2022-09-14 22:55:46 +0300

    Create pywerf2.py

---
+++ pywerf2.py
@@ -0,0 +1,39 @@
+import subprocess
+from threading import Thread
+
+def check_process(process):
+    while process_events.stdout.readable():
+        line = process_events.stdout.readline()
+        print(line.strip())
+
+
+process_events = subprocess.Popen('kubectl get events -A -o wide -w',
+                shell=True,
+                universal_newlines=True,
+                encoding='utf-8',
+                bufsize=1,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+
+process_pods = subprocess.Popen('kubectl get pods -A -o wide -w',
+                shell=True,
+                universal_newlines=True,
+                encoding='utf-8',
+                bufsize=1,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+
+
+
+t_pods = Thread(target=check_process, args=(process_pods,))
+t_pods.start()
+
+t_events = Thread(target=check_process, args=(process_events,))
+t_events.start()
+
+
+process_pods.wait()
+process_pods.join()
+
+process_events.wait()
+process_events.join()
